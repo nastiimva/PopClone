d69a69832fc93c609c9ad8ffa6adcfdc5d4ddc79
nastiimva <nastiimva@mail.ru>
2023-10-20 23:18:08 +0300

    Шарики появляются и летят вверх

---
+++ Assets/Scripts/BalloonBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonBehaviour : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    public float balloonGravity = -2.00f;
+    private float _gravity = -9.81f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gravity = _gravity * balloonGravity * Vector3.up;
+        _rigidbody.AddForce(gravity, ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Scripts/SpawnBalloons.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalloons : MonoBehaviour
+{
+    public GameObject balloon;
+
+    public Material[] balloonColors;
+
+    private float maxX, minX, maxZ, minZ;
+
+    private Coroutine spawnBalloon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxX = transform.position.x + transform.localScale.x / 2;
+        minX = transform.position.x - transform.localScale.x / 2;
+        
+        maxZ = transform.position.z + transform.localScale.z / 2;
+        minZ = transform.position.z - transform.localScale.z / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnBalloon == null)
+            spawnBalloon = StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+        Material spawnMaterial = balloonColors[Random.Range(0,balloonColors.Length)];
+        var gameObject = Instantiate(balloon, spawnPosition, Quaternion.identity);
+        gameObject.GetComponent<MeshRenderer>().material = spawnMaterial;
+        yield return new WaitForSeconds(1f);
+        spawnBalloon = null;
+    }
+}
